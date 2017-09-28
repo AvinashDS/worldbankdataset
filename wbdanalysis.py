bf9fb41650996d5b981c615bbb352396d9afb102
TheAvinashSingh <avinash.singh130413@gmail.com>
2017-09-29 02:09:29 +0530

    added country name in place of country code in title and plototng data of BRICS countries

--- wbdanalysis.py
+++ wbdanalysis.py
@@ -9,6 +9,25 @@ import matplotlib.pyplot as plt
 
 def plot_pop(filename, country_code):
 
+	#To get the Country Name for displaying it in Plot in the Title Field
+	#-------------------------------------------------------------------
+
+	#Reading CSV as a whole
+	wbd = pd.read_csv(fn)
+
+	#Getting List of Indexes where that Country Code is present in the DataSet
+	list_index_country = wbd.index.values[wbd['CountryCode']== country_code]
+	index_country = list_index_country[0]
+
+	#Getting the Name and Index of the country and converting them to a dictionary
+	location = wbd.iloc[[index_country],[0]]
+	location_dict = dict(location)
+
+	#Using key Values in Dictionary Extracting the Country Name and storing it into cname
+	[(k,v)] = location_dict.items()
+	cname = v.values[0]
+	#-------------------------------------------------------------------
+
 	#Reading Large data in Chunks of 1000 pieces
 	urb_pop_reader = pd.read_csv(filename, chunksize=1000)
 
@@ -31,12 +50,15 @@ def plot_pop(filename, country_code):
 		data = data.append(df_pop_ceb)
 
 	#Plotting the Graph for the Population Change over the span of 50 years
-	data.plot(kind='scatter', x='Year', y='Total Urban Population', title= country_code)
+	data.plot(kind='scatter', x='Year', y='Total Urban Population', title= cname)
 
 	plt.show()
 
 fn = 'world_ind_pop_data.csv'
 
+#Showing Plots of BRICS Countries Data of 50 years
+plot_pop(fn, 'BRA')
+plot_pop(fn, 'RUS')
 plot_pop(fn, 'IND')
-
 plot_pop(fn, 'CHN')
+plot_pop(fn, 'ZAF')
